6dce79e9ec5c7ef01520a1655ad36d8be85ec60c
1stNIIICE <66973235+17thNIIICE@users.noreply.github.com>
2022-06-10 19:59:57 +0300

    jquery

--- js/main.js
+++ js/main.js
@@ -7,120 +7,35 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
-//FORM VALID
-document.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
 
-    async function formSend(e) {
-        e.preventDefault();
+  
+// //POPUP-WINDOW via JQuery
 
-        let error = formValidate(form);
-
-    }
-
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._required');
-
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
-
-            if(input.classList.contains('_email')){
-                if (emailTest(input)){
-                    formAddError(input);
-                    error++;
-                }
-            }else if(input.classList.contains('_phoneNumber')){
-                if (phoneNumberTest(input)){
-                    formAddError(input);
-                    error++
-                }
-            }else {
-                if (input.value === '') {
-                    formAddError(input);
-                    error++;
-                }
-            }
-        }
-    }
-
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error')
-    }
-    function formRemoveError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
-    //EMAIL TEST
-    function emailTest(input){
-        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-    }
-    //PHONE NUMBER TEST
-    function phoneNumberTest(input){
-        return !/^\d[\d\(\)\ -]{4,12}\d$/.test(input.value);
-    }
+$(document).ready(function(){
+    //open popup
+    $("#popup-link").click(function(){
+        $("#popup").show(400);
+    })
+    //close popup
+    $("#close-button").click(function(){
+        $("#popup").hide(200);
+    })
 })
 
-//POPUP-WINDOW'es
-const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll('.lock-padding');
-
-
-let unlock = true;
-
-const timeout = 700;
-
-if (popupLinks.length > 0) {
-    for (let index = 0; index < popupLinks.length; index++) {
-        const popupLink = popupLinks[index];
-        popupLink.addEventListener("click", function (e) {
-            const popupName = popupLink.getAttribute('href').replace('#', '');
-            const currentPopup = document.getElementById(popupName);
-            popupOpen(currentPopup);
-            e.preventDefault();
-        }) 
-    }
-}
-
-const popupCloseIcon = document.querySelectorAll('.close-popup__button');
-if (popupCloseIcon.length > 0) {
-    for (let index = 0; index < popupCloseIcon.length; index++){
-    const element = popupCloseIcon[index];
-    element.addEventListener('click', function (e) {
-            popupClose(element.closest('.popup-window'));
-            e.preventDefault();
-        });
-    }
-}
-
-function popupOpen(currentPopup) {
-    if (currentPopup && unlock){
-        const popupActive = document.querySelector('.popup-window.open');
-        if (popupActive) {
-                popupClose(popupActive, false);
-        }
-        currentPopup.classList.add('open');
-        currentPopup.addEventListener('click', function (e) {
-            if (!e.target.closest('.popup-window__content')) {
-                popupClose(e.target.closest('.popup-window'));
-            }
-        })
-    }
-}
-
-function popupClose(popupActive, doUnlock = true) {
-    if (unlock) {
-        popupActive.classList.remove('open');
-    }
-}
-document.addEventListener('keydown', function (e){
-    if (e.which === 27) {
-        const popupActive = document.querySelector('.popup-window.open');
-        popupClose(popupActive);
-    }
+//Formatting phone number via Cleave
+$(document).ready(function(){
+    var cleave = new Cleave('.input-phone', {
+        phone: true,
+        phoneRegionCode: 'BY',
+        blocks: [2, 3, 2, 2],
+        delimiter: '-'
+    });
+    $('#form').parsley().on('field:validated', function(){
+        var ok = $('parsley-error').Length === 0;
+    }).on('form:submit', function(){
+        return false;
+    })
 })
+
+
 AOS.init();
