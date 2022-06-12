40e4da07fd70840e5aab902a590a71f0b8d83bc0
1stNIIICE <66973235+17thNIIICE@users.noreply.github.com>
2022-06-12 15:21:10 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 "use strict"
-var swiper = new Swiper(".mySwiper", {
-    speed: 1000,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-  
 // //POPUP-WINDOW via JQuery
-
 $(document).ready(function(){
+    //Swiper slider
+    var swiper = new Swiper(".mySwiper", {
+        speed: 1000,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+    });
+    //Cleave 
+    var cleave = new Cleave('.input-phone', {
+        phone: true,
+        phoneRegionCode: 'BY',
+        blocks: [2, 3, 2, 2],
+        delimiter: '-'
+    });
     //open popup
     $("#popup-link").click(function(){
         $("#popup").show(400);
@@ -20,22 +25,11 @@ $(document).ready(function(){
     $("#close-button").click(function(){
         $("#popup").hide(200);
     })
-})
-
-//Formatting phone number via Cleave
-$(document).ready(function(){
-    var cleave = new Cleave('.input-phone', {
-        phone: true,
-        phoneRegionCode: 'BY',
-        blocks: [2, 3, 2, 2],
-        delimiter: '-'
-    });
+    //Parsley
     $('#form').parsley().on('field:validated', function(){
         var ok = $('parsley-error').Length === 0;
     }).on('form:submit', function(){
         return false;
     })
-})
-
-
+});
 AOS.init();
